1d5c980616b67820841b361283600dd10e263cc3
Bayani Carbone <bayani.carbone@canonnical.com>
2021-08-10 14:11:30 +0200

    Add numactl to nova_compute_utils.py base packages
    This tool is useful for numa usage reporting.
    
    Change-Id: Id0aaba514b06fe3dfa79e9e1d263b1dbde20f494

--- hooks/nova_compute_utils.py
+++ hooks/nova_compute_utils.py
@@ -136,6 +136,7 @@ BASE_PACKAGES = [
     'xfsprogs',
     'nfs-common',
     'open-iscsi',
+    'numactl',
     'python3-novaclient',  # lib required by juju actions
     'python3-neutronclient',  # lib required by juju actions
     'python3-keystoneauth1',  # lib required by juju actions
